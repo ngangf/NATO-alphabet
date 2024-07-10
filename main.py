58a87be3638f2c6735a1f3b04e901b97447d8218
Foriben Ngang <Foriben2006@gmail.com>
2024-07-10 19:34:15 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas
 
-_data = pandas.read_csv("nato_phonetic_alphabet.csv")
-phonetic_dict = {row.letter:row.code for (index, row) in _data.iterrows()}
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 word = input("Enter a word: ").upper()
 output_list = [phonetic_dict[letter] for letter in word]
